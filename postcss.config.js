5ec930df356e9aebe68a285ea21bf289dbce78fe
wangjie19 <wangjie19@baidu.com>
2018-02-08 17:18:25 +0800

    modify-some

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ module.exports = {
     // 自定义样式文件才使用sugarss；我们用的是.css文件所以不用这个配置
     // parser: 'sugarss',
     plugins: {
-        'postcss-cssnext': {}
+        'postcss-cssnext': {
+            browsers: ['last 2 versions', '> 5%'],
+        }
     }
 }
